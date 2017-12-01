fa309c3d4c9196f67a72a1d687ce49308ec1a806
Vedran Biđin <vedran.bidin@gmail.com>
2017-12-01 21:52:34 +0100

    Authentication modified - users can now login with their username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,35 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
-         :validatable, :confirmable, :lockable
+         :validatable, :confirmable, :lockable,
+         :authentication_keys => [:login]
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 end
